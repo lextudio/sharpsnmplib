5ad10371c65881a166672cdb77cc022f5567e09d
lextm <none@none>
2009-01-10 04:32:47 +0000

    Missing files added
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4017908

---
+++ Browser/OutputPanelTraceListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Lextm.SharpSnmpLib.Browser
+{
+    public class OutputPanelTraceListener: TraceListener
+    {
+        private static OutputPanel _panel;
+
+        internal static OutputPanel Panel
+        {
+            set { _panel = value; }
+        }
+
+        public override void Write(string message)
+        {
+            // leave this empty so event level information is not logged in the OutputPanel.
+        }
+
+        public override void WriteLine(string message)
+        {
+            if (_panel == null)
+            {
+                return;
+            }
+
+            _panel.WriteLine(message);
+        }
+    }
+}
