62b47cc65e88d292fad186ce617e4ba8bdd8bde3
lextm <none@none>
2009-01-18 03:14:00 +0000

    First Silverlight 2 port attempt. Failed with so many missing classes.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4018034

--- SharpSnmpLib/AssemblyInfo.cs
+++ SharpSnmpLib/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.010114.01")]
+[assembly: AssemblyVersion("1.5.010118.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("1.5.010114.01")]
+[assembly: AssemblyFileVersion("1.5.010118.00")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
