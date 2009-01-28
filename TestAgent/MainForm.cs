73ca2cd6d9b714a51c13d359b8727da6bba63a79
lextm <none@none>
2009-01-28 08:39:23 +0000

    TestAgent demo now can send out TRAP and INFORM messages.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4018176

--- TestAgent/MainForm.cs
+++ TestAgent/MainForm.cs
@@ -9,65 +9,106 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Net;
 using System.Windows.Forms;
+
 using Lextm.SharpSnmpLib;
 
 namespace TestAgent
 {
-    /// <summary>
-    /// Description of MainForm.
-    /// </summary>
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-            UpdateButtons(false);
-        }
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			UpdateButtons(false);
+		}
 
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            agent1.Monitor.Start(161);
-            UpdateButtons(true);
-        }
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			agent1.Monitor.Start(161);
+			UpdateButtons(true);
+		}
 
-        private void UpdateButtons(bool started)
-        {
-            btnStart.Enabled = !started;
-            btnStop.Enabled = started;
-        }
+		private void UpdateButtons(bool started)
+		{
+			btnStart.Enabled = !started;
+			btnStop.Enabled = started;
+		}
 
-        private void btnStop_Click(object sender, EventArgs e)
-        {
-            agent1.Monitor.Stop();
-            UpdateButtons(false);
-        }
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			agent1.Monitor.Stop();
+			UpdateButtons(false);
+		}
 
-        private void agent1_GetRequestReceived(object sender, Lextm.SharpSnmpLib.GetRequestReceivedEventArgs e)
-        {
-            GetRequestMessage message = e.Request;
-            // you may validate message version number and/or community name here.
-            if (message.Variables.Count != 1)
-            {
-                return;
-            }
-            if (message.Variables[0].Id != sysDescr)
-            {
-                return;
-            }
+		private void agent1_GetRequestReceived(object sender, Lextm.SharpSnmpLib.GetRequestReceivedEventArgs e)
+		{
+			GetRequestMessage message = e.Request;
+			// you may validate message version number and/or community name here.
+			if (message.Variables.Count != 1)
+			{
+				return;
+			}
+			if (message.Variables[0].Id != sysDescr)
+			{
+				return;
+			}
 
-            List<Variable> list = new List<Variable>();
-            list.Add(new Variable(sysDescr, new OctetString("Test Description")));
+			List<Variable> list = new List<Variable>();
+			list.Add(new Variable(sysDescr, new OctetString("Test Description")));
 
-            GetResponseMessage response = new GetResponseMessage(message.SequenceNumber, message.Version, e.Sender.Address, message.Community, list);
-            response.Send(e.Sender.Port);
-        }
+			GetResponseMessage response = new GetResponseMessage(message.SequenceNumber, message.Version, e.Sender.Address, message.Community, list);
+			response.Send(e.Sender.Port);
+		}
 
-        private static ObjectIdentifier sysDescr = new ObjectIdentifier("1.3.6.1.2.1.1.1.0");
+		private static ObjectIdentifier sysDescr = new ObjectIdentifier("1.3.6.1.2.1.1.1.0");
 
-        private void agent1_ExceptionRaised(object sender, ExceptionRaisedEventArgs e)
-        {
-            MessageBox.Show(e.Exception.Message);
-        }
-    }
+		private void agent1_ExceptionRaised(object sender, ExceptionRaisedEventArgs e)
+		{
+			MessageBox.Show(e.Exception.Message);
+		}
+		
+		private void BtnTrapClick(object sender, EventArgs e)
+		{
+			Agent.SendTrapV1(new IPEndPoint(IPAddress.Parse(txtIP.Text), int.Parse(txtPort.Text)),
+			                 IPAddress.Loopback, // here should be IP of the current machine.
+			                 new OctetString("public"),
+			                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+			                 GenericCode.ColdStart,
+			                 0,
+			                 0,
+			                 new List<Variable>());
+		}
+		
+		private void BtnTrap2Click(object sender, EventArgs e)
+		{
+			Agent.SendTrapV2(VersionCode.V2,
+			                 new IPEndPoint(IPAddress.Parse(txtIP.Text), int.Parse(txtPort.Text)),
+			                 new OctetString("public"),
+			                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+			                 0,
+			                 new List<Variable>());
+		}
+		
+		private void BtnInformClick(object sender, EventArgs e)
+		{
+			try
+			{
+				Agent.SendInform(VersionCode.V2, 
+				                 new IPEndPoint(IPAddress.Parse(txtIP.Text), int.Parse(txtPort.Text)),
+				                 new OctetString("public"),
+				                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+				                 0,
+				                 new List<Variable>(), 2000);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
 }
