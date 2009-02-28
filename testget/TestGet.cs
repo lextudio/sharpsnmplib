d1cd025a4a91239a0f5b5e8deffd284816e426b2
lextm <none@none>
2009-02-28 11:34:35 +0000

    Latest version of Mono.Options is used. Slightly update snmpget.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4018664

--- testget/TestGet.cs
+++ testget/TestGet.cs
@@ -1,31 +1,42 @@
 using System;
-using System.Net;
-using Lextm.SharpSnmpLib;
-using System.Text;
 using System.Collections.Generic;
-using NDesk.Options;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Lextm.SharpSnmpLib;
+using Mono.Options;
 
 class TestGet
 {
-    // get system.sysLocation on localhost
     static void Main(string[] args)
     {
         string community = "public";
         bool show_help   = false;
+        bool show_version = false;
         VersionCode version = VersionCode.V1;
-        int timeout = 1000;
+        int timeout = 1000; 
+        int retry = 0;
 
-        OptionSet p = new OptionSet ()
-            .Add ("c:", delegate (string v) { if (v != null) community = v; })
-            .Add ("h|?|help",  delegate (string v) { show_help = v != null; })
-            .Add ("t:", delegate (string v) { timeout = int.Parse(v) * 1000; })
-            .Add ("V|version:",   delegate (string v) { version = (VersionCode)Enum.Parse(typeof(VersionCode), v, true); });
+        OptionSet p = new OptionSet()
+            .Add("c:", "-c for community name, (default is public)", delegate (string v) { if (v != null) community = v; })
+            .Add("h|?|help", "-h, -?, -help for help.", delegate (string v) { show_help = v != null; })
+            .Add("v", "-v to display version number of this application.", delegate (string v) { show_version = v != null; })
+            .Add("t:", "-t for timeout value (unit is second).", delegate (string v) { timeout = int.Parse(v) * 1000; })
+            .Add("r:", "-r for retry count (default is 0)", delegate (string v) { retry = int.Parse(v); })
+            .Add("V|version:", "-V or -version for SNMP version (v1, v2 are currently supported)", delegate (string v) { version = (VersionCode)Enum.Parse(typeof(VersionCode), v, true); });
         
         List<string> extra = p.Parse (args);
         
         if (show_help)
         {
             Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
+            return;
+        }
+        
+        if (show_version)
+        {
+            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
             return;
         }
 
@@ -35,17 +46,31 @@ class TestGet
             return;
         }
         
-        string ip = extra[0];
-        List<Variable> vList = new List<Variable>();
-        for (int i = 1; i < extra.Count; i++)
+        IPAddress ip;
+        bool parsed = IPAddress.TryParse(extra[0], out ip);
+        if (!parsed)
         {
-            Variable test = new Variable(new ObjectIdentifier(extra[i]));
-            vList.Add(test);
+            ip = IPAddress.Loopback;
+            foreach (IPAddress address in Dns.GetHostAddresses(extra[0]))
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ip = address;
+                    break;
+                }
+            }
         }
         
         try
-        {            
-            foreach (Variable variable in Manager.Get(version, new IPEndPoint(IPAddress.Parse(ip), 161), new OctetString(community), vList, timeout))
+        {
+            List<Variable> vList = new List<Variable>();
+            for (int i = 1; i < extra.Count; i++)
+            {
+                Variable test = new Variable(new ObjectIdentifier(extra[i]));
+                vList.Add(test);
+            }
+            
+            foreach (Variable variable in Manager.Get(version, new IPEndPoint(ip, 161), new OctetString(community), vList, timeout))
             {
                 Console.WriteLine(variable);
             }
