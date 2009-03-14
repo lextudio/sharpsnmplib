d50eb384a3d08e1a859c8ae356f71a85f902dd1d
lextm <none@none>
2009-03-14 12:45:35 +0000

    TestSet sample updated.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4018797

--- testget/TestGet.cs
+++ testget/TestGet.cs
@@ -2,92 +2,97 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-
 using Lextm.SharpSnmpLib;
 using Mono.Options;
 
-class TestGet
+namespace TestGet
 {
-    static void Main(string[] args)
+    class TestGet
     {
-        string community = "public";
-        bool show_help   = false;
-        bool show_version = false;
-        VersionCode version = VersionCode.V1;
-        int timeout = 1000; 
-        int retry = 0;
+        static void Main(string[] args)
+        {
+            string community = "public";
+            bool show_help   = false;
+            bool show_version = false;
+            VersionCode version = VersionCode.V1;
+            int timeout = 1000; 
+            int retry = 0;
 
-        OptionSet p = new OptionSet()
-            .Add("c:", "-c for community name, (default is public)", delegate (string v) { if (v != null) community = v; })
-            .Add("h|?|help", "-h, -?, -help for help.", delegate (string v) { show_help = v != null; })
-            .Add("v", "-v to display version number of this application.", delegate (string v) { show_version = v != null; })
-            .Add("t:", "-t for timeout value (unit is second).", delegate (string v) { timeout = int.Parse(v) * 1000; })
-            .Add("r:", "-r for retry count (default is 0)", delegate (string v) { retry = int.Parse(v); })
-            .Add("V|version:", "-V or -version for SNMP version (v1, v2 are currently supported)", delegate (string v) { version = (VersionCode)Enum.Parse(typeof(VersionCode), v, true); });
+            OptionSet p = new OptionSet()
+                .Add("c:", "-c for community name, (default is public)", delegate (string v) { if (v != null) community = v; })
+                .Add("h|?|help", "-h, -?, -help for help.", delegate (string v) { show_help = v != null; })
+                .Add("v", "-v to display version number of this application.", delegate (string v) { show_version = v != null; })
+                .Add("t:", "-t for timeout value (unit is second).", delegate (string v) { timeout = int.Parse(v) * 1000; })
+                .Add("r:", "-r for retry count (default is 0)", delegate (string v) { retry = int.Parse(v); })
+                .Add("V|version:", "-V or -version for SNMP version (v1, v2 are currently supported)", delegate (string v) { version = (VersionCode)Enum.Parse(typeof(VersionCode), v, true); });
         
-        List<string> extra = p.Parse (args);
+            List<string> extra = p.Parse (args);
         
-        if (show_help)
-        {
-            Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
-            return;
-        }
-        
-        if (show_version)
-        {
-            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
-            return;
-        }
-
-        if (extra.Count < 2)
-        {
-            Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
-            return;
-        }
-        
-        IPAddress ip;
-        bool parsed = IPAddress.TryParse(extra[0], out ip);
-        if (!parsed)
-        {
-            foreach (IPAddress address in Dns.GetHostAddresses(extra[0]))
+            if (show_help)
             {
-                if (address.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    ip = address;
-                    break;
-                }
-            }
-            
-            if (ip == null)
-            {
-                Console.WriteLine("invalid host or wrong IP address found: " + extra[0]);
+                Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
                 return;
             }
-        }
         
-        try
-        {            
-            for (int i = 1; i < extra.Count; i++)
+            if (show_version)
+            {
+                Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+                return;
+            }
+
+            if (extra.Count < 2)
+            {
+                Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
+                return;
+            }
+        
+            IPAddress ip;
+            bool parsed = IPAddress.TryParse(extra[0], out ip);
+            if (!parsed)
+            {
+                foreach (IPAddress address in Dns.GetHostAddresses(extra[0]))
+                {
+                    if (address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ip = address;
+                        break;
+                    }
+                }
+            
+                if (ip == null)
+                {
+                    Console.WriteLine("invalid host or wrong IP address found: " + extra[0]);
+                    return;
+                }
+            }
+
+            try
             {
                 List<Variable> vList = new List<Variable>();
-                Variable test = new Variable(new ObjectIdentifier(extra[i]));
-                vList.Add(test);
-                Variable variable = Manager.Get(version, new IPEndPoint(ip, 161), new OctetString(community), vList, timeout)[0];
-                Console.WriteLine(variable);
+                for (int i = 1; i < extra.Count; i++)
+                {
+                    Variable test = new Variable(new ObjectIdentifier(extra[i]));
+                    vList.Add(test);
+                }
+
+                foreach (
+                    Variable variable in
+                        Manager.Get(version, new IPEndPoint(ip, 161), new OctetString(community), vList, timeout))
+                {
+                    Console.WriteLine(variable);
+                }
             }
-        }
-        catch (SharpSnmpException ex)
-        {
-            if (ex is SharpOperationException)
+            catch (SharpSnmpException ex)
             {
-                Console.WriteLine((ex as SharpOperationException).Details);
-            }
-            else
-            {
-                Console.WriteLine(ex);
+                if (ex is SharpOperationException)
+                {
+                    Console.WriteLine((ex as SharpOperationException).Details);
+                }
+                else
+                {
+                    Console.WriteLine(ex);
+                }
             }
         }
     }
 }
-
--- testset/Program.cs
+++ testset/Program.cs
@@ -7,32 +7,138 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Net;
-using Lextm.SharpSnmpLib;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Lextm.SharpSnmpLib;
+using Mono.Options;
 
 namespace TestSet
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			try
-			{
-				Variable test = new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.6.0"), new OctetString("Beijing"));
+    class TestSet
+    {
+        static void Main(string[] args)
+        {
+            string community = "public";
+            bool show_help = false;
+            bool show_version = false;
+            VersionCode version = VersionCode.V1;
+            int timeout = 1000;
+            int retry = 0;
+
+            OptionSet p = new OptionSet()
+                .Add("c:", "-c for community name, (default is public)", delegate(string v) { if (v != null) community = v; })
+                .Add("h|?|help", "-h, -?, -help for help.", delegate(string v) { show_help = v != null; })
+                .Add("v", "-v to display version number of this application.", delegate(string v) { show_version = v != null; })
+                .Add("t:", "-t for timeout value (unit is second).", delegate(string v) { timeout = int.Parse(v) * 1000; })
+                .Add("r:", "-r for retry count (default is 0)", delegate(string v) { retry = int.Parse(v); })
+                .Add("V|version:", "-V or -version for SNMP version (v1, v2 are currently supported)", delegate(string v) { version = (VersionCode)Enum.Parse(typeof(VersionCode), v, true); });
+
+            List<string> extra = p.Parse(args);
+
+            if (show_help)
+            {
+                Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
+                return;
+            }
+
+            if (show_version)
+            {
+                Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+                return;
+            }
+
+            if (extra.Count < 2)
+            {
+                Console.WriteLine("The syntax is similar to Net-SNMP. http://www.net-snmp.org/docs/man/snmpget.html");
+                return;
+            }
+
+            IPAddress ip;
+            bool parsed = IPAddress.TryParse(extra[0], out ip);
+            if (!parsed)
+            {
+                foreach (IPAddress address in Dns.GetHostAddresses(extra[0]))
+                {
+                    if (address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ip = address;
+                        break;
+                    }
+                }
+
+                if (ip == null)
+                {
+                    Console.WriteLine("invalid host or wrong IP address found: " + extra[0]);
+                    return;
+                }
+            }
+
+            if ((extra.Count - 1) % 3 != 0)
+            {
+                Console.WriteLine("invalid variable number: " + (extra.Count - 1));
+                return;
+            }
+
+            try
+            {
                 List<Variable> vList = new List<Variable>();
-                vList.Add(test);
+                for (int i = 1; i < extra.Count; i = i + 3)
+                {
+                    string type = extra[i + 1];
+                    if (type.Length != 1)
+                    {
+                        Console.WriteLine("invalid type string: " + type);
+                        return;
+                    }
 
-                Manager.Set(VersionCode.V1, new IPEndPoint(IPAddress.Loopback, 161), new OctetString("private"), vList, 5000);
-			    Manager.Set(VersionCode.V2, new IPEndPoint(IPAddress.Loopback, 161), new OctetString("private"), vList, 5000);
-			}
-			catch (SharpOperationException ex)
-			{
-				Console.WriteLine(ex);
-			}
+                    ISnmpData data;
 
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
-	}
+                    switch (type[0])
+                    {
+                        case 'i':
+                            data = new Integer32(int.Parse(extra[i + 2]));
+                            break;
+                        case 'u':
+                            // TODO: add Counter64, Gauge support later.
+                            data = new Counter32(uint.Parse(extra[i + 2]));
+                            break;
+                        case 't':
+                            data = new TimeTicks(uint.Parse(extra[i + 2]));
+                            break;
+                        case 'a':
+                            data = new IP(IPAddress.Parse(extra[i + 2]));
+                            break;
+                        case 'o':
+                            data = new ObjectIdentifier(extra[i + 2]);
+                            break;
+                        case 's':
+                        case 'x':
+                        case 'd':
+                            data = new OctetString(extra[i + 2]);
+                            break;
+                        default :
+                            Console.WriteLine("unknown type string: " + type[0]);
+                            return;
+                    }
+
+                    Variable test = new Variable(new ObjectIdentifier(extra[i]), data);
+                    vList.Add(test);
+                }
+
+                Manager.Set(version, new IPEndPoint(ip, 161), new OctetString(community), vList, timeout);
+            }
+            catch (SharpSnmpException ex)
+            {
+                if (ex is SharpOperationException)
+                {
+                    Console.WriteLine((ex as SharpOperationException).Details);
+                }
+                else
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
 }
