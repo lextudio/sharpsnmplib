3973d3c9a4571b753d6dc182a3b7ce75194b0957
lextm <none@none>
2009-04-25 12:53:40 +0000

    --HG-- extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4019291

--- SharpSnmpLib/AssemblyInfo.cs
+++ SharpSnmpLib/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.010419.02")]
+[assembly: AssemblyVersion("2.0.010425.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("1.9.010419.02")]
+[assembly: AssemblyFileVersion("2.0.010425.00")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
