fe4c1c21999647193c6ba96d5d54c0bb7a9510ba
lextm <none@none>
2009-05-17 12:32:06 +0000

    CF project updated. Will continue next weekend.
    --HG--
    rename : SharpSnmpLib/Resource.resx => Browser/Resource.resx
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4019561

--- SharpSnmpLib/AssemblyInfo.cs
+++ SharpSnmpLib/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.010517.100")]
+[assembly: AssemblyVersion("2.1.010517.107")]
 #if (!CF)
-[assembly: AssemblyFileVersion("2.1.010517.100")]
+[assembly: AssemblyFileVersion("2.1.010517.107")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
