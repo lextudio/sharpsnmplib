8617273861e5e5d22f74384092034db6c6c7d6a4
lextm <none@none>
2009-10-02 10:09:51 +0000

    Work item 5020 closed.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4027379

--- testgettable/Program.cs
+++ testgettable/Program.cs
@@ -18,9 +18,9 @@ namespace TestGetTable
                 Variable[,] table = Messenger.GetTable(VersionCode.V1, new IPEndPoint(IPAddress.Loopback, 161), new OctetString("public"),
                     new ObjectIdentifier(new uint[] { 1, 3, 6, 1, 2, 1, 2, 2 }), 5000, 10, DefaultObjectRegistry.Instance);
                 writer.WriteLine("V1 table");
-                for (int row = 0; row < table.GetUpperBound(0); row++)
+                for (int row = 0; row <= table.GetUpperBound(0); row++)
                 {
-                    for (int col = 0; col < table.GetUpperBound(1); col++)
+                    for (int col = 0; col <= table.GetUpperBound(1); col++)
                     {
                         writer.Write(table[row, col].Data + ", ");
                     }
@@ -30,9 +30,9 @@ namespace TestGetTable
                 table = Messenger.GetTable(VersionCode.V2, new IPEndPoint(IPAddress.Loopback, 161), new OctetString("public"),
                     new ObjectIdentifier(new uint[] { 1, 3, 6, 1, 2, 1, 2, 2 }), 5000, 10, DefaultObjectRegistry.Instance);
                 writer.WriteLine("V2 table");
-                for (int row = 0; row < table.GetUpperBound(0); row++)
+                for (int row = 0; row <= table.GetUpperBound(0); row++)
                 {
-                    for (int col = 0; col < table.GetUpperBound(1); col++)
+                    for (int col = 0; col <= table.GetUpperBound(1); col++)
                     {
                         writer.Write(table[row, col].Data + ", ");
                     }
