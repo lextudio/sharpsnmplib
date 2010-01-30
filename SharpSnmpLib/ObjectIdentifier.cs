30f6d5cad6692c9b39eec3b4d93a97b7c13435bc
lextm <none@none>
2010-01-30 05:12:10 +0000

    Version number changed to 4.0. Will release 4.0 Beta 1 based on this build.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4034515

--- SharpSnmpLib/ObjectIdentifier.cs
+++ SharpSnmpLib/ObjectIdentifier.cs
@@ -125,10 +125,26 @@ namespace Lextm.SharpSnmpLib
             return _oid;
         }
 
+        /// <summary>
+        /// Compares the current object with another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// A 32-bit signed integer that indicates the relative order of the objects being compared. The return value has the following meanings:
+        /// Value
+        /// Meaning
+        /// Less than zero
+        /// This object is less than the <paramref name="other"/> parameter.
+        /// Zero
+        /// This object is equal to <paramref name="other"/>.
+        /// Greater than zero
+        /// This object is greater than <paramref name="other"/>.
+        /// </returns>
         public int Compare(ObjectIdentifier other)
         {
             return CompareTo(other);
         }
+        
         /// <summary>
         /// Compares the current object with another object of the same type.
         /// </summary>
