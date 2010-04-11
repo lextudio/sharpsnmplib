18a36e708819bef5452249bb703f34fdf1e27d1f
lextm <none@none>
2010-04-11 07:03:38 +0000

    MonoDevelop removed AssemblyInfo Task from project files. Now added that back.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4039577

--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("4.1.010410.05")]
+[assembly: AssemblyVersion("4.2.010411.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("4.1.010410.05")]
+[assembly: AssemblyFileVersion("4.2.010411.00")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
