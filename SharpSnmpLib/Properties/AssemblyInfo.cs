819140c353348ecb15fe63b84dec12a1ac03717e
lextm <none@none>
2010-05-01 01:15:31 +0000

    Fixed snmptrapd.vbproj for .NET 3.5/VS2008.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4043398

--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010425.08")]
+[assembly: AssemblyVersion("5.0.010501.01")]
 #if (!CF)
-[assembly: AssemblyFileVersion("5.0.010425.08")]
+[assembly: AssemblyFileVersion("5.0.010501.01")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
