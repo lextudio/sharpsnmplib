522432409c0c5055d406b3c1e2fe46447e3a7cc2
lextm <none@none>
2010-05-01 12:26:01 +0000

    Tested on .NET and Mono. Will use log4net to replace System.Diagnostics for logging in browser and compiler.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4043480

--- Compiler/Properties/AssemblyInfo.cs
+++ Compiler/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010501.06")]
-[assembly: AssemblyFileVersion("5.0.010501.06")]
+[assembly: AssemblyVersion("5.0.010501.07")]
+[assembly: AssemblyFileVersion("5.0.010501.07")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
