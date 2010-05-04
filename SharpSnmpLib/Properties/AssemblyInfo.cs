225ae7fdf4ef41767a0273552939b4e62787d5c9
lextm <none@none>
2010-05-04 06:02:24 +0000

    Signed all binaries. Removed garbage.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4043989

--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010504.03")]
+[assembly: AssemblyVersion("5.0.010504.05")]
 #if (!CF)
-[assembly: AssemblyFileVersion("5.0.010504.03")]
+[assembly: AssemblyFileVersion("5.0.010504.05")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
