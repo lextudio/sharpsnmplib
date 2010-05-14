f01cf06693dff308faf9e2e435983d038077d79f
lextm <none@none>
2010-05-14 12:20:59 +0000

    Created a translate utility. Will enhance it later.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4045003

--- Browser/Properties/AssemblyInfo.cs
+++ Browser/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010514.02")]
-[assembly: AssemblyFileVersion("5.0.010514.02")]
+[assembly: AssemblyVersion("5.0.010514.03")]
+[assembly: AssemblyFileVersion("5.0.010514.03")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
 
---
+++ Samples/VB.NET/snmptranslate/Program.vb
@@ -0,0 +1,17 @@
+﻿Imports Lextm.SharpSnmpLib.Mib
+Imports Lextm.SharpSnmpLib
+
+Module Program
+
+    Sub Main(ByVal args As String())
+        If args.Length <> 1 Then
+            Console.WriteLine("This application takes one parameter.")
+        End If
+
+        Dim oid As String = args(0)
+        Dim registry As New ReloadableObjectRegistry("modules")
+        Dim textual As String = registry.Translate(ObjectIdentifier.Convert(oid))
+        Console.WriteLine(textual)
+    End Sub
+
+End Module
