623be398faf9dfd26ddec589d704f1822c4f8b20
lextm <none@none>
2010-05-14 12:21:55 +0000

    Added the C# version of snmptranslate.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4045004

---
+++ Samples/C#/snmptranslate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Mib;
+
+namespace snmptranslate
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("This application takes one parameter.");
+            }
+
+            string oid = args[0];
+            ReloadableObjectRegistry registry = new ReloadableObjectRegistry("modules");
+            string textual = registry.Translate(ObjectIdentifier.Convert(oid));
+            Console.WriteLine(textual);
+        }
+    }
+}
