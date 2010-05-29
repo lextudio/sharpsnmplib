a0f926223df9378ba21caa48483342eb52ab32eb
lextm <none@none>
2010-05-29 06:23:34 +0000

    Updated version number and now 5.0 final is ready to ship.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4046253

--- Compiler/Properties/AssemblyInfo.cs
+++ Compiler/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010523.05")]
-[assembly: AssemblyFileVersion("5.0.010523.05")]
+[assembly: AssemblyVersion("5.0.010529.00")]
+[assembly: AssemblyFileVersion("5.0.010529.00")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
 
