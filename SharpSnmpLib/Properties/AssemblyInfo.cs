2868793aeef4690be3f86e767319487f3220a039
lextm <none@none>
2010-05-29 06:44:54 +0000

    Fixed version number.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4046256

--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.010523.03")]
+[assembly: AssemblyVersion("5.0.010529.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("5.0.010523.03")]
+[assembly: AssemblyFileVersion("5.0.010529.00")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
