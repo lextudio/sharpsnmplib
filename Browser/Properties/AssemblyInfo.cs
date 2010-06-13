b87d5dabe497412df65e4b226de594a375f0830a
lextm <none@none>
2010-06-13 04:27:21 +0000

    Use Mono files to make CF project complete. Still need to be tested.
    --HG--
    extra : convert_revision : svn%3A0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5%4047107

--- Browser/Properties/AssemblyInfo.cs
+++ Browser/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("5.1.010612.00")]
-[assembly: AssemblyFileVersion("5.1.010612.00")]
+[assembly: AssemblyVersion("5.1.010612.02")]
+[assembly: AssemblyFileVersion("5.1.010612.02")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
 
