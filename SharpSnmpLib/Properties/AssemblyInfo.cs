550fe4c263501aea3419cc48a339921c1b4fa2ab
lextm <none@none>
2010-10-05 18:26:09 +0800

    Source code management bindings modified.

--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -47,9 +47,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("6.0.011003.28")]
+[assembly: AssemblyVersion("6.0.011005.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("6.0.011003.28")]
+[assembly: AssemblyFileVersion("6.0.011005.00")]
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
