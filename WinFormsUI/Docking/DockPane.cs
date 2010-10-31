a452c2955202d4117da2d27c0fd77d0427f57a69
lextm <none@none>
2010-10-31 17:30:38 +0800

    Merged DockPanel Suite 2.4 code base into #SNMP.

--- WinFormsUI/Docking/DockPane.cs
+++ WinFormsUI/Docking/DockPane.cs
@@ -493,7 +493,10 @@ namespace WeifenLuo.WinFormsUI.Docking
 				return;
 
 			if (content.DockHandler.HideOnClose)
+			{
 				content.DockHandler.Hide();
+				NestedDockingStatus.NestedPanes.Remove(this);
+			}
 			else
 				content.DockHandler.Close();
 
@@ -589,7 +592,7 @@ namespace WeifenLuo.WinFormsUI.Docking
 
             CaptionControl.RefreshChanges();
             TabStripControl.RefreshChanges();
-            if (DockState == DockState.Float)
+            if (DockState == DockState.Float && FloatWindow != null)
                 FloatWindow.RefreshChanges();
             if (DockHelper.IsDockStateAutoHide(DockState) && DockPanel != null)
             {
@@ -1161,6 +1164,7 @@ namespace WeifenLuo.WinFormsUI.Docking
 		public void RestoreToPanel()
 		{
             DockPanel.SuspendLayout(true);
+
 			IDockContent activeContent = DockPanel.ActiveContent;
 
 			for (int i=DisplayingContents.Count-1; i>=0; i--)
