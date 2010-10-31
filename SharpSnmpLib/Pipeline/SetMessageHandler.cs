1f0fc3e1245a29e23919bbc0105e885ab24eb402
lextm <none@none>
2010-10-31 12:01:43 +0800

    SetMessageHandler added.

---
+++ SharpSnmpLib/Pipeline/SetMessageHandler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lextm.SharpSnmpLib.Pipeline
+{
+    /// <summary>
+    /// SET message handler.
+    /// </summary>    
+    /// <remarks>
+    /// Follows RFC 3416 4.2.1.
+    /// Not supported scenarios are, 
+    ///   wrongLength, 
+    ///   wrongEncoding, 
+    ///   wrongValue, 
+    ///   noCreation,
+    ///   inconsistentName,
+    ///   inconsistentValue,
+    ///   resourceUnavailable,
+    ///   commitFailed,
+    ///   undoFailed
+    /// </remarks>
+    // ReSharper disable UnusedMember.Global
+    public class SetMessageHandler : IMessageHandler
+    // ReSharper restore UnusedMember.Global
+    {
+        /// <summary>
+        /// Handles the specified message.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="store">The object store.</param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public void Handle(SnmpContext context, ObjectStore store)
+        {
+            int index = 0;
+            ErrorCode status = ErrorCode.NoError;
+
+            IList<Variable> result = new List<Variable>();
+            foreach (Variable v in context.Request.Pdu.Variables)
+            {
+                index++;
+                ScalarObject obj = store.GetObject(v.Id);
+                if (obj != null)
+                {
+                    try
+                    {
+                        obj.Data = v.Data;
+                    }
+                    catch (AccessFailureException)
+                    {
+                        status = ErrorCode.NoAccess;
+                    }
+                    catch (ArgumentException)
+                    {
+                        status = ErrorCode.WrongType;
+                    }
+                    catch (Exception)
+                    {
+                        status = ErrorCode.GenError;
+                    }
+                }
+                else
+                {
+                    status = ErrorCode.NotWritable;
+                }
+
+                if (status != ErrorCode.NoError)
+                {
+                    context.CopyRequest(status, index);
+                    return;
+                }
+
+                result.Add(v);
+            }
+
+            context.GenerateResponse(result);
+        }
+    }
+}
