5480e5ca64a6dbe1586c7ea14beb18d2df841b7b
lextm <none@none>
2010-11-19 21:54:38 +0800

    Fixed Sandcastle project.

--- Compiler/Properties/AssemblyInfo.cs
+++ Compiler/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("7.0.011118.03")]
-[assembly: AssemblyFileVersion("7.0.011118.03")]
+[assembly: AssemblyVersion("7.0.011119.00")]
+[assembly: AssemblyFileVersion("7.0.011119.00")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
 
