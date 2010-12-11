23af5af339ca6db5a6779800568a47d7c819012b
lextm <none@none>
2010-12-11 08:49:25 +0800

    Renamed timeout in Discoverer.Discover to interval. Fixed its doc tag.

--- Compiler/Properties/AssemblyInfo.cs
+++ Compiler/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("7.0.011206.04")]
-[assembly: AssemblyFileVersion("7.0.011206.04")]
+[assembly: AssemblyVersion("7.0.011211.01")]
+[assembly: AssemblyFileVersion("7.0.011211.01")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
 
