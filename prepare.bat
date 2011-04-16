978979433e2ee9af5dc77aca1e38671fe2f9e167
lextm <none@none>
2011-04-16 19:13:08 +0800

    Simplified prepare.bat and prepare.sh.

--- prepare.bat
+++ prepare.bat
@@ -1,2 +1,1 @@
-copy SharpSnmpLib\sharpsnmplib.snk.bak SharpSnmpLib\sharpsnmplib.snk 
-copy SharpSnmpLib\Properties\AssemblyInfo.cs.bak SharpSnmpLib\Properties\AssemblyInfo.cs
+copy SharpSnmpLib\sharpsnmplib.snk.bak SharpSnmpLib\sharpsnmplib.snk
--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,1 @@
 cp SharpSnmpLib/sharpsnmplib.snk.bak SharpSnmpLib/sharpsnmplib.snk
-cp SharpSnmpLib/Properties/AssemblyInfo.cs.bak SharpSnmpLib/Properties/AssemblyInfo.cs
