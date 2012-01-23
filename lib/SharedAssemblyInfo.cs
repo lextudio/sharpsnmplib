b682b1fff11009442864ff446ca80f44c5307051
lextm@lextm-win7 <lextm@lextm-win7>
2012-01-23 12:00:50 +0800

    Fixed all solutions and projects. Since System.Tuples is under MIT, included its source files in SharpSnmpLib project directly.
    --HG--
    rename : System.Tuples/ConversionsGenerator.cs => SharpSnmpLib/System.Tuples/ConversionsGenerator.cs
    rename : System.Tuples/ConversionsGenerator.tt => SharpSnmpLib/System.Tuples/ConversionsGenerator.tt
    rename : System.Tuples/NormalTuple.ttinclude => SharpSnmpLib/System.Tuples/NormalTuple.ttinclude
    rename : System.Tuples/Settings.ttinclude => SharpSnmpLib/System.Tuples/Settings.ttinclude
    rename : System.Tuples/TupleGenerator.cs => SharpSnmpLib/System.Tuples/TupleGenerator.cs
    rename : System.Tuples/TupleGenerator.tt => SharpSnmpLib/System.Tuples/TupleGenerator.tt
    rename : System.Tuples/Util.ttinclude => SharpSnmpLib/System.Tuples/Util.ttinclude
    rename : System.Tuples/ZeroTuple.ttinclude => SharpSnmpLib/System.Tuples/ZeroTuple.ttinclude

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("7.5.010122.35")]
+[assembly: AssemblyVersion("7.5.010123.28")]
 #if (!CF)
-[assembly: AssemblyFileVersion("7.5.010122.35")]
+[assembly: AssemblyFileVersion("7.5.010123.28")]
 #endif
--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("7.5.010122.35")> 
-<Assembly: AssemblyFileVersion("7.5.010122.35")> 
+<Assembly: AssemblyVersion("7.5.010123.28")> 
+<Assembly: AssemblyFileVersion("7.5.010123.28")> 
