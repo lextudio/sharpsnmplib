69df537fd93aa42402d28f76b01e0db9dcc42afd
Lex Li <support@lextm.com>
2012-01-27 16:39:26 +0800

    Fixed ignore file. Fixed grammar file.

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("7.5.010124.20")> 
-<Assembly: AssemblyFileVersion("7.5.010124.20")> 
+<Assembly: AssemblyVersion("7.5.010127.03")> 
+<Assembly: AssemblyFileVersion("7.5.010127.03")> 
