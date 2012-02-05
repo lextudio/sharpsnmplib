1f644f9cd18de91d811aa0fd688b12ab556498ed
Lex Li <support@lextm.com>
2012-02-05 18:23:44 +0800

    Added license statement for the new compiler. BSD 3 Clause is used.

--- SharpSnmpLib/Mib/CompilerError.cs
+++ SharpSnmpLib/Mib/CompilerError.cs
@@ -1,3 +1,29 @@
+// Copyright (c) 2008-2012, Lex Li
+// All rights reserved.
+//   
+// Redistribution and use in source and binary forms, with or without modification, are 
+// permitted provided that the following conditions are met:
+//   
+// - Redistributions of source code must retain the above copyright notice, this list 
+//   of conditions and the following disclaimer.
+//   
+// - Redistributions in binary form must reproduce the above copyright notice, this list
+//   of conditions and the following disclaimer in the documentation and/or other materials 
+//   provided with the distribution.
+//   
+// - Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to 
+//   endorse or promote products derived from this software without specific prior written 
+//   permission.
+//   
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &AS IS& AND ANY EXPRESS 
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 /*
  * Created by SharpDevelop.
  * User: lextm
@@ -8,6 +34,7 @@
  */
 
 using System;
+using System.Text;
 using Antlr.Runtime;
 
 namespace Lextm.SharpSnmpLib.Mib
@@ -18,15 +45,28 @@ namespace Lextm.SharpSnmpLib.Mib
     [Serializable]
     public class CompilerError
     {
-        private readonly RecognitionException _error;
-
         /// <summary>
         /// Creates a <see cref="CompilerError"/> instance with a specific <see cref="Exception"/>.
         /// </summary>
         /// <param name="exception">Compiler exception.</param>
         public CompilerError(RecognitionException exception)
         {
-            _error = exception;
+            Token = exception.Token;
+            var result = new StringBuilder();
+            if (!string.IsNullOrEmpty(FileName))
+            {
+                result.AppendFormat("{0} ({1},{2}) : ", FileName, Token.Line, Token.CharPositionInLine + 1);
+            }
+
+            result.AppendFormat("error S0001 : invalid token {0}", Token.Text);
+            Details = result.ToString();
+        }
+
+        public CompilerError(IToken token, string fileName, string details)
+        {
+            Token = token;
+            FileName = fileName;
+            Details = details;
         }
 
         /// <summary>
@@ -38,26 +78,10 @@ namespace Lextm.SharpSnmpLib.Mib
             return "CompilerError: " + Details;
         }
 
-        public string Details
-        {
-            get
-            {
-                var token = _error.Token;
-                return string.IsNullOrEmpty(FileName)
-                           ? string.Format(
-                               "error S0001 : invalid token {0}", token.Text)
-                           : string.Format(
-                               "{0} ({1},{2}) : error S0001 : invalid token {3}",
-                               FileName,
-                               token.Line, token.CharPositionInLine + 1, token.Text);
-            }
-        }
+        public string Details { get; set; }
 
         public string FileName { private get; set; }
 
-        public IToken Token
-        {
-            get { return _error.Token; }
-        }
+        public IToken Token { get; set; }
     }
 }
