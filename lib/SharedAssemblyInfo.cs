de58f7c050a0976654e7ee4b90a9217e99af6ae3
Lex Li <support@lextm.com>
2012-06-10 20:23:44 +0800

    Compiled the help documentation.

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("7.5.010429.10")]
+[assembly: AssemblyVersion("7.5.010510.01")]
 #if (!CF)
-[assembly: AssemblyFileVersion("7.5.010429.10")]
+[assembly: AssemblyFileVersion("7.5.010510.01")]
 #endif
