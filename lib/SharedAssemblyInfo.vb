f938047e37d3457bb7665f881e3be018087819a3
Lex Li <support@lextm.com>
2012-08-04 09:37:41 +0800

    Added EngineGroupTestFixture.cs。

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("7.5.010728.09")> 
-<Assembly: AssemblyFileVersion("7.5.010728.09")> 
+<Assembly: AssemblyVersion("7.5.010804.02")> 
+<Assembly: AssemblyFileVersion("7.5.010804.02")> 
