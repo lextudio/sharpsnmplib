018681f56cc74adb034dd47c8d60cfdeac3e90da
Lex Li <support@lextm.com>
2012-08-12 17:37:59 +0800

    Updated NuGet packages.

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("7.5.010804.09")> 
-<Assembly: AssemblyFileVersion("7.5.010804.09")> 
+<Assembly: AssemblyVersion("7.5.010812.02")> 
+<Assembly: AssemblyFileVersion("7.5.010812.02")> 
