3d51122fd3d97a8b73e0473774d752ef527eb6f3
Lex Li <support@lextm.com>
2013-01-28 07:30:21 -0500

    Upgraded NuGet to 2.2. Fixed NuGet issues under Mono.

--- nuget.bat
+++ nuget.bat
@@ -3,6 +3,9 @@ call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:clean /p:Configuration=Release /p:
 call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\net35\
 call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\net40\
 call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:build /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:OutputPath=..\bin\net40\
+call %MSBuildDir%\msbuild SharpSnmpLib.cf35.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\net35-cf\
+call %MSBuildDir%\msbuild SharpSnmpLib.cf35.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\net35-cf\
+
 md .nuget
 cd .nuget
 call nuget pack
