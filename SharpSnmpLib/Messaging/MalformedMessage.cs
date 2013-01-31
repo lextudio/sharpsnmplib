254ddf312020de3b8e736eb6ff0b79273ec1b116
Lex Li <support@lextm.com>
2013-01-31 10:39:46 +0800

    Library license changed from Lesser GPL to MIT/X11. Added EncryptedScope property in MalformedMessage.

--- SharpSnmpLib/Messaging/MalformedMessage.cs
+++ SharpSnmpLib/Messaging/MalformedMessage.cs
@@ -1,19 +1,21 @@
 // Malformed message type.
 // Copyright (C) 2010 Lex Li.
 // 
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
 // 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
 // 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 using System;
 using System.Globalization;
@@ -33,7 +35,19 @@ namespace Lextm.SharpSnmpLib.Messaging
         /// </summary>
         /// <param name="messageId">The message id.</param>
         /// <param name="user">The user.</param>
+        [Obsolete("Please use the new overloading constructor.")]
         public MalformedMessage(int messageId, OctetString user)
+            : this(messageId, user, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MalformedMessage"/> class.
+        /// </summary>
+        /// <param name="messageId">The message id.</param>
+        /// <param name="user">The user.</param>
+        /// <param name="data">The data encrypted.</param>
+        public MalformedMessage(int messageId, OctetString user, ISnmpData data)
         {
             if (user == null)
             {
@@ -43,7 +57,14 @@ namespace Lextm.SharpSnmpLib.Messaging
             Header = new Header(messageId);
             Parameters = SecurityParameters.Create(user);
             Scope = DefaultScope;
+            EncryptedScope = data;
         }
+
+        /// <summary>
+        /// Encrypted scope data.
+        /// </summary>
+        /// <value>The original scope data from packet, which remains encrypted.</value>
+        public ISnmpData EncryptedScope { get; set; }
 
         /// <summary>
         /// Gets the parameters.
@@ -54,7 +75,7 @@ namespace Lextm.SharpSnmpLib.Messaging
         /// <summary>
         /// Gets the scope.
         /// </summary>
-        /// <value>The scope.</value>
+        /// <value>The fake scope, which is used to avoid exceptions in message handling.</value>
         public Scope Scope
         {
             get; private set;
--- SharpSnmpLib/Properties/AssemblyInfo.cs
+++ SharpSnmpLib/Properties/AssemblyInfo.cs
@@ -1,19 +1,21 @@
-// <summary>#SNMP Library. An open source SNMP implementation for .NET.</summary>
-// <copyright company="Lex Y. Li" file="AssemblyInfo.cs">Copyright (C) 2008  Lex Y. Li</copyright>
+// <summary>#SNMP Library. An open source SNMP implementation for .NET and Mono.</summary>
+// <copyright company="Lex Li" file="AssemblyInfo.cs">Copyright (C) 2008  Lex Li</copyright>
 // 
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
 // 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
 // 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 #region Using directives
 
@@ -29,11 +31,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SharpSnmpLib")]
-[assembly: AssemblyDescription("#SNMP Library for .NET")]
+[assembly: AssemblyDescription("#SNMP Library for .NET/Mono")]
 [assembly: AssemblyConfiguration("Lesser GPL 2.1+")]
 [assembly: AssemblyCompany("LeXtudio")]
 [assembly: AssemblyProduct("#SNMPLib")]
-[assembly: AssemblyCopyright("(C) 2008-2012 Malcolm Crowe, Lex Li, Steve Santacroce, and other contributors.")]
+[assembly: AssemblyCopyright("(C) 2008-2012 Malcolm Crowe, Lex Li, and other contributors.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
