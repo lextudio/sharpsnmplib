bccad03582db926f24155facc6a869391b3108df
Lex Li <support@lextm.com>
2013-12-28 21:22:25 +0800

    Fixed snmpd project.

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("7.6.011210.11")> 
-<Assembly: AssemblyFileVersion("7.6.011210.11")> 
+<Assembly: AssemblyVersion("7.6.011228.04")> 
+<Assembly: AssemblyFileVersion("7.6.011228.04")> 
