ca403a19c3b99768abfcdc66e5b3970c721a7c86
Lex Li <support@lextm.com>
2014-02-16 14:08:20 +0800

    Prepared 8.5 alpha release.

--- nuget.bat
+++ nuget.bat
@@ -1,11 +1,10 @@
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\net35\
-call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\net35\
 call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\net40\
 call %MSBuildDir%\msbuild SharpSnmpLib.sln /t:build /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:OutputPath=..\bin\net40\
-call %MSBuildDir%\msbuild SharpSnmpLib.cf35.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\net35-cf\
-call %MSBuildDir%\msbuild SharpSnmpLib.cf35.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\net35-cf\
-
+call %MSBuildDir%\msbuild SharpSnmpLib.iOS.sln /t:clean /p:Configuration=Release /p:Platform=iPhone /p:OutputPath=..\bin\Monotouch\
+call %MSBuildDir%\msbuild SharpSnmpLib.iOS.sln /t:build /p:Configuration=Release /p:Platform=iPhone /p:OutputPath=..\bin\Monotouch\
+call %MSBuildDir%\msbuild SharpSnmpLib.Android.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\Monoandroid\
+call %MSBuildDir%\msbuild SharpSnmpLib.Android.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\Monoandroid\
 md .nuget
 cd .nuget
 call nuget pack
