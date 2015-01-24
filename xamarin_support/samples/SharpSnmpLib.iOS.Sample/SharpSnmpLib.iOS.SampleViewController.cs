d3caee6984061e2db17dc048cef5449a29dc2a05
Lex Li <support@lextm.com>
2015-01-24 11:19:43 +0800

    Upgraded iOS projects to latest Xamarin.iOS.

--- xamarin_support/samples/SharpSnmpLib.iOS.Sample/SharpSnmpLib.iOS.SampleViewController.cs
+++ xamarin_support/samples/SharpSnmpLib.iOS.Sample/SharpSnmpLib.iOS.SampleViewController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Drawing;
 
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using Foundation;
+using UIKit;
 using System.Net;
 using Lextm.SharpSnmpLib;
 using System.Collections.Generic;
