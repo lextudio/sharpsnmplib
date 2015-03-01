404f564135c4c2f5c44d4cdb79e7efbfa859ccbf
Lex Li <support@lextm.com>
2015-03-01 15:01:25 +0800

    Fixed the Windows build by adding NuGet restore.

--- debug.bat
+++ debug.bat
@@ -1,4 +1,5 @@
 set EnableNuGetPackageRestore=true
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+.nuget\NuGet.exe restore SharpSnmpLib.sln
 call %MSBuildDir%\msbuild Build.proj /t:debug
 @IF %ERRORLEVEL% NEQ 0 PAUSE
