87fcfdb7f84b6c38bfcb2c7a99866fcdc561c92c
Lex Li <support@lextm.com>
2015-08-08 18:26:48 +0800

    Added initial UWP support.

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("8.5.010228.01")> 
-<Assembly: AssemblyFileVersion("8.5.010228.01")> 
+<Assembly: AssemblyVersion("8.9.010719.03")> 
+<Assembly: AssemblyFileVersion("8.9.010719.03")> 
