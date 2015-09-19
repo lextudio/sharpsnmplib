e145bb841c87c469009fdce313f9c6e49d8ac73f
Lex Li <support@lextm.com>
2015-09-19 13:58:48 +0800

    Added one more TRAP v2 message example.

--- Samples/CSharp/snmpsendtrap/Program.cs
+++ Samples/CSharp/snmpsendtrap/Program.cs
@@ -17,35 +17,48 @@ namespace SnmpSendTrap
 {
     using Lextm.SharpSnmpLib.Security;
 
-    internal static class Program 
+    internal static class Program
     {
         public static void Main(string[] args)
         {
             IPAddress address = args.Length == 1 ? IPAddress.Parse(args[0]) : IPAddress.Loopback;
 
-            Messenger.SendTrapV1(new IPEndPoint(address, 162), IPAddress.Loopback,
-                                 new OctetString("public"),
-                                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
-                                 GenericCode.ColdStart,
-                                 0,
-                                 0,
-                                 new List<Variable>());
+            Messenger.SendTrapV1(
+                new IPEndPoint(address, 162),
+                IPAddress.Loopback,
+                new OctetString("public"),
+                new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+                GenericCode.ColdStart,
+                0,
+                0,
+                new List<Variable>());
 
             //Thread.Sleep(50);
 
 
-            Messenger.SendTrapV2(0, VersionCode.V2, new IPEndPoint(address, 162),
-                                 new OctetString("public"),
-                                 new ObjectIdentifier(new uint[] { 1, 3, 6 }),
-                                 0,
-                                 new List<Variable>());
+            Messenger.SendTrapV2(
+                0,
+                VersionCode.V2,
+                new IPEndPoint(address, 162),
+                new OctetString("public"),
+                new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+                0,
+                new List<Variable>());
             //Thread.Sleep(50);
 
             try
             {
-                Messenger.SendInform(0, VersionCode.V2, new IPEndPoint(address, 162), new OctetString("public"), new ObjectIdentifier(new uint[] { 1, 3, 6 }),
-                                     0,
-                                     new List<Variable>(), 2000, null, null);
+                Messenger.SendInform(
+                    0,
+                    VersionCode.V2,
+                    new IPEndPoint(address, 162),
+                    new OctetString("public"),
+                    new ObjectIdentifier(new uint[] { 1, 3, 6 }),
+                    0,
+                    new List<Variable>(),
+                    2000,
+                    null,
+                    null);
             }
             catch (Exception ex)
             {
@@ -74,22 +87,41 @@ namespace SnmpSendTrap
                 Console.WriteLine(ex);
             }
 
-            var trap = new TrapV2Message(
-                VersionCode.V3,
-                528732060,
+            {
+                var trap = new TrapV2Message(
+                    VersionCode.V3,
+                    528732060,
+                    1905687779,
+                    new OctetString("neither"),
+                    new ObjectIdentifier("1.3.6"),
+                    0,
+                    new List<Variable>(),
+                    DefaultPrivacyProvider.DefaultPair,
+                    0x10000,
+                    new OctetString(ByteTool.Convert("80001F8880E9630000D61FF449")),
+                    0,
+                    0);
+                trap.Send(new IPEndPoint(address, 162));
+            }
 
-                1905687779,
-                new OctetString("neither"),
-                new ObjectIdentifier("1.3.6"),
-                0,
-                new List<Variable>(),
-                DefaultPrivacyProvider.DefaultPair,
-                0x10000,
-                new OctetString(ByteTool.Convert("80001F8880E9630000D61FF449")),
-                0,
-                0
-               );
-            trap.Send(new IPEndPoint(address, 162));
+            {
+                var trap = new TrapV2Message(
+                    VersionCode.V3,
+                    528732060,
+                    1905687779,
+                    new OctetString("privacy"),
+                    new ObjectIdentifier("1.3.6"),
+                    0,
+                    new List<Variable>(),
+                    new DESPrivacyProvider(
+                        new OctetString("privacyphrase"),
+                        new MD5AuthenticationProvider(new OctetString("authentication"))),
+                    0x10000,
+                    new OctetString(ByteTool.Convert("80001F8880E9630000D61FF449")),
+                    0,
+                    0);
+                trap.Send(new IPEndPoint(address, 162));
+            }
         }
     }
 }
