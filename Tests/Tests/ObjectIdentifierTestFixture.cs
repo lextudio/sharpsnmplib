1aae25981503e781b622697899bbe1b66b256555
Kristian Virkus <kristian.virkus@gmail.com>
2015-10-07 11:09:13 +0200

    Fixed unit tests based on the test runner's OS language.

--- Tests/Tests/ObjectIdentifierTestFixture.cs
+++ Tests/Tests/ObjectIdentifierTestFixture.cs
@@ -90,7 +90,7 @@ namespace Lextm.SharpSnmpLib.Tests
         public void TestUuid()
         {
             var exception = Assert.Throws<ArgumentException>(() => new ObjectIdentifier("2.25.329800735698586629295641978511506172918"));
-			Assert.AreEqual(string.Format("Parameter 329800735698586629295641978511506172918 is out of 32 bit unsigned integer range{0}Parametername: dotted", Environment.NewLine), exception.Message);
+			Assert.IsTrue(exception.Message.StartsWith("Parameter 329800735698586629295641978511506172918 is out of 32 bit unsigned integer range"));
         }
 
         [Test]
