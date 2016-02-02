0f6fc22fa48543e12600ca3295c12942817fc3de
Lex Li <support@lextm.com>
2016-02-02 18:10:24 +0800

    Fixed batch files.

--- dist.nuget.bat
+++ dist.nuget.bat
@@ -1,8 +1,4 @@
-rmdir /S /Q bin
-mkdir bin
-set msBuildDir=C:\Program Files (x86)\MSBuild\12.0\Bin
-call "%MSBuildDir%\msbuild" SharpSnmpLib.vs2013.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\
-call "%MSBuildDir%\msbuild" SharpSnmpLib.vs2013.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\
+call release.bat
 md .nuget
 cd .nuget
 call nuget pack
--- release.bat
+++ release.bat
@@ -1,4 +1,16 @@
-set EnableNuGetPackageRestore=true
-set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %MSBuildDir%\msbuild Build.proj /t:release
+:: http://stackoverflow.com/questions/328017/path-to-msbuild
+:: http://www.csharp411.com/where-to-find-msbuild-exe/
+:: http://timrayburn.net/blog/visual-studio-2013-and-msbuild/
+:: http://blogs.msdn.com/b/visualstudio/archive/2013/07/24/msbuild-is-now-part-of-visual-studio.aspx
+for %%v in (2.0, 3.5, 4.0, 12.0, 14.0) do (
+  for /f "usebackq tokens=2* delims= " %%c in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\%%v" /v MSBuildToolsPath`) do (
+    set msBuildExe=%%dMSBuild.exe
+  )
+)
+
+rmdir /S /Q bin
+mkdir bin
+.nuget\nuget.exe restore
+call "%MSBuildExe%" SharpSnmpLib.sln /t:clean /p:Configuration=Release /p:OutputPath=..\bin\
+call "%MSBuildExe%" SharpSnmpLib.sln /t:build /p:Configuration=Release /p:OutputPath=..\bin\
 @IF %ERRORLEVEL% NEQ 0 PAUSE
