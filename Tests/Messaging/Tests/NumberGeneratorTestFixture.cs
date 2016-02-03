f4136304e63a994114556009f9844f165b691e25
Lex Li <support@lextm.com>
2016-02-03 20:49:37 +0800

    Added a NumberGenerator test case.

---
+++ Tests/Messaging/Tests/NumberGeneratorTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Lextm.SharpSnmpLib.Messaging.Tests
+{
+    public class NumberGeneratorTestFixture
+    {
+        [Fact]
+        public void Test()
+        {
+            var ng = new NumberGenerator(int.MinValue, int.MaxValue);
+            var list = Enumerable.Range(1, 1000000).AsParallel().Select(x => ng.NextId);
+
+            var dupes = list.GroupBy(x => x).Where(g => g.Count() > 1).Select(x => x.Key);
+
+            Assert.Equal(0, dupes.Count());
+        }
+    }
+}
