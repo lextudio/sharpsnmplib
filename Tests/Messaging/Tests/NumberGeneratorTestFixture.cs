63e46226635deeaabddb3fc5de9bb7d625026876
Lex Li <support@lextm.com>
2016-02-04 21:08:36 +0800

    Added test case.

--- Tests/Messaging/Tests/NumberGeneratorTestFixture.cs
+++ Tests/Messaging/Tests/NumberGeneratorTestFixture.cs
@@ -1,19 +1,41 @@
 ﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Lextm.SharpSnmpLib.Messaging.Tests
 {
     public class NumberGeneratorTestFixture
     {
-        [Fact]
-        public void Test()
+        [Theory]
+        [InlineData(1000000)]
+        public void Test(int count)
         {
             var ng = new NumberGenerator(int.MinValue, int.MaxValue);
-            var list = Enumerable.Range(1, 1000000).AsParallel().Select(x => ng.NextId);
-
+            ng.SetSalt(0);
+            var list = Enumerable.Range(1, count).AsParallel().Select(x => ng.NextId);
             var dupes = list.GroupBy(x => x).Where(g => g.Count() > 1).Select(x => x.Key);
-
             Assert.Equal(0, dupes.Count());
+        }
+
+        [Theory]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void TestOverflow(int count)
+        {
+            var overallDupes = 0;
+            Parallel.ForEach(Enumerable.Range(0, count), i =>
+            {
+                var ng = new NumberGenerator(int.MinValue, int.MaxValue);
+                ng.SetSalt(int.MaxValue - 5);
+                var list = Enumerable.Range(1, 20).AsParallel().Select(x => ng.NextId);
+                var dupes = list.GroupBy(x => x).Where(g => g.Count() > 1).Select(x => x.Key);
+                if (dupes.Count() > 0)
+                {
+                    Interlocked.Increment(ref overallDupes);
+                }
+            });
+            Assert.Equal(0, overallDupes);
         }
     }
 }
