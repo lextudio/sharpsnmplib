c3385a223c3674685f2f2960ed7f9cad0588bc7e
Lex Li <support@lextm.com>
2017-09-04 22:05:39 -0400

    Moved .NET Framework projects to a separate solution with NuGet package reference.

--- Samples/CSharp/BytesViewer/Form1.cs
+++ Samples/CSharp/BytesViewer/Form1.cs
@@ -2,13 +2,6 @@
 using Lextm.SharpSnmpLib.Messaging;
 using Lextm.SharpSnmpLib.Security;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BytesViewer
