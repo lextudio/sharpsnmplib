7911406c9022497c5cccb15b58f364749dec6278
Lex Li <support@lextm.com>
2017-09-26 12:17:44 -0400

    Revised Travis build scripts.

--- test.travis.sh
+++ test.travis.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+set -e
+
+export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
+
 cd Tests
 cd CSharpCore
 dotnet test --filter "FullyQualifiedName~Lextm.SharpSnmpLib.Integration"
