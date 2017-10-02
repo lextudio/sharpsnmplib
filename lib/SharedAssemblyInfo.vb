1496bf1bfc2ad7d1ee0c7a66af3ab14faec021b6
Lex Li <support@lextm.com>
2017-10-01 21:49:59 -0400

    Bumped to 10.0.1 final.

--- lib/SharedAssemblyInfo.vb
+++ lib/SharedAssemblyInfo.vb
@@ -7,5 +7,5 @@ Imports System.Reflection
 ' You can specify all values by your own or you can build default build and revision
 ' numbers with the '*' character (the default):
 
-<Assembly: AssemblyVersion("9.1.000425.90")> 
-<Assembly: AssemblyFileVersion("9.1.000425.90")> 
+<Assembly: AssemblyVersion("10.0.1.0")> 
+<Assembly: AssemblyFileVersion("10.0.1.0")> 
