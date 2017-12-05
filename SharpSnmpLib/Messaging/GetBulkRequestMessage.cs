b8c5ccefd594311f9798ea28cdee62c9731049a2
Nestor Reyes <nreyes@printfleet.com>
2017-12-05 16:36:33 -0500

    Obsolete constructor calls new one with an empty octet string for context name

--- SharpSnmpLib/Messaging/GetBulkRequestMessage.cs
+++ SharpSnmpLib/Messaging/GetBulkRequestMessage.cs
@@ -205,68 +205,9 @@ namespace Lextm.SharpSnmpLib.Messaging
         /// <param name="maxMessageSize">Size of the max message.</param>
         /// <param name="report">The report.</param>
         [Obsolete("Please use other overloading ones.")]
-        public GetBulkRequestMessage(VersionCode version, int messageId, int requestId, OctetString userName, int nonRepeaters, int maxRepetitions, IList<Variable> variables, IPrivacyProvider privacy, int maxMessageSize, ISnmpMessage report)
+        public GetBulkRequestMessage(VersionCode version, int messageId, int requestId, OctetString userName, int nonRepeaters, int maxRepetitions, IList<Variable> variables, IPrivacyProvider privacy, int maxMessageSize, ISnmpMessage report) 
+            : this(version, messageId, requestId, userName,new OctetString(string.Empty), nonRepeaters, maxRepetitions, variables, privacy, maxMessageSize, report)
         {
-            if (variables == null)
-            {
-                throw new ArgumentNullException(nameof(variables));
-            }
-            
-            if (userName == null)
-            {
-                throw new ArgumentNullException(nameof(userName));
-            }
-            
-            if (version != VersionCode.V3)
-            {
-                throw new ArgumentException("Only v3 is supported.", nameof(version));
-            }
-
-            if (report == null)
-            {
-                throw new ArgumentNullException(nameof(report));
-            }
-            
-            if (privacy == null)
-            {
-                throw new ArgumentNullException(nameof(privacy));
-            }
-
-            if (nonRepeaters > variables.Count)
-            {
-                throw new ArgumentException("nonRepeaters should not be greater than variable count.", nameof(nonRepeaters));
-            }
-
-            if (maxRepetitions < 1)
-            {
-                throw new ArgumentException("maxRepetitions should be greater than 0.", nameof(maxRepetitions));
-            }
-
-            Version = version;
-            Privacy = privacy;
-            
-            // TODO: define more constants.
-            Header = new Header(new Integer32(messageId), new Integer32(maxMessageSize), privacy.ToSecurityLevel() | Levels.Reportable);
-            var parameters = report.Parameters;
-            var authenticationProvider = Privacy.AuthenticationProvider;
-            Parameters = new SecurityParameters(
-                parameters.EngineId,
-                parameters.EngineBoots,
-                parameters.EngineTime,
-                userName,
-                authenticationProvider.CleanDigest,
-                Privacy.Salt);
-            var pdu = new GetBulkRequestPdu(
-                requestId,
-                nonRepeaters,
-                maxRepetitions,
-                variables);
-            var scope = report.Scope;
-            var contextEngineId = scope.ContextEngineId == OctetString.Empty ? parameters.EngineId : scope.ContextEngineId;
-            Scope = new Scope(contextEngineId, scope.ContextName, pdu);
-
-            Privacy.ComputeHash(Version, Header, Parameters, Scope);
-            _bytes = this.PackMessage(null).ToBytes();
         }
 
         internal GetBulkRequestMessage(VersionCode version, Header header, SecurityParameters parameters, Scope scope, IPrivacyProvider privacy, byte[] length)
