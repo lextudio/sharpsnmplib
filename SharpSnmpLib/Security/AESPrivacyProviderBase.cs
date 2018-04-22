74c1fc4aa96e25753ba4e39f426914ec5c722d05
Lex Li <support@lextm.com>
2018-04-21 20:55:38 -0400

    Revised testing platforms.

--- SharpSnmpLib/Security/AESPrivacyProviderBase.cs
+++ SharpSnmpLib/Security/AESPrivacyProviderBase.cs
@@ -53,8 +53,6 @@ namespace Lextm.SharpSnmpLib.Security
             {
 #if NETSTANDARD1_3
                 return false;
-#elif NETSTANDARD2_0
-                return false;
 #elif NETCOREAPP2_0
                 return false;
 #else
