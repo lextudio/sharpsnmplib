7f01fc3d4c18d5b424bdd63292f9e5c3dea31177
Lex Li <support@lextm.com>
2018-07-17 16:39:29 -0400

    Bumped the version.

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("10.0.8.0")]
-[assembly: AssemblyFileVersion("10.0.8.0")]
+[assembly: AssemblyVersion("10.0.9.0")]
+[assembly: AssemblyFileVersion("10.0.9.0")]
