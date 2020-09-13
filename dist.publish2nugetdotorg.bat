bcd94bd85d632d6643ecdc0c4ed49152f39a2ae2
Lex Li <support@lextm.com>
2020-09-13 16:44:01 -0400

    Removed netcoreapp3.0.

--- dist.publish2nugetdotorg.bat
+++ dist.publish2nugetdotorg.bat
@@ -1,2 +1,2 @@
-f:\nuget.exe update /self
-for %%f in (.\*.nupkg) do f:\nuget.exe push %%f -Source https://www.nuget.org/api/v2/package
+..\nuget.exe update /self
+for %%f in (.\*.nupkg) do ..\nuget.exe push %%f -Source https://www.nuget.org/api/v2/package
