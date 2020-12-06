ad74f31712733388b49449b2d8c48f455cc8406a
Lex Li <support@lextm.com>
2020-12-06 12:54:10 -0500

    Added cert test.

--- sign.assembly.ps1
+++ sign.assembly.ps1
@@ -1,3 +1,18 @@
+$file = ".\cert.txt"
+if (-not (Test-Path $file))
+{
+    Write-Host "No certificate specified. Exit."
+    exit 0
+}
+
+$cert = Get-Content -Path $file -TotalCount 1
+$foundCert = Test-Certificate -Cert $cert -User
+if(!$foundCert)
+{
+    Write-Host "Certificate doesn't exist. Exit."
+    exit 0
+}
+
 $signtool="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\signtool.exe"
 if (Test-Path $signtool) {
     Write-Output "sign the assembly"
