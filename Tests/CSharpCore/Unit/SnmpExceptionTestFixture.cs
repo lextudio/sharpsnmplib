f2909d5d468750ea47c02d1a32fa40650bd13aa4
Lex Li <support@lextm.com>
2023-04-08 03:34:36 -0400

    Updated test cases.

---
+++ Tests/CSharpCore/Unit/SnmpExceptionTestFixture.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Xunit;
+
+namespace Lextm.SharpSnmpLib.Tests
+{
+    public class SnmpExceptionTests
+    {
+        [Fact]
+        public void SnmpException_SerializationTest()
+        {
+            // Arrange
+            var originalException = new SnmpException("Test message", new InvalidOperationException());
+            var binaryFormatter = new BinaryFormatter();
+
+            // Act
+            // Serialize the original exception to a memory stream
+            using var memoryStream = new MemoryStream();
+            binaryFormatter.Serialize(memoryStream, originalException);
+            memoryStream.Position = 0;
+
+            // Deserialize the memory stream back into an object
+            var deserializedException = (SnmpException)binaryFormatter.Deserialize(memoryStream);
+
+            // Assert
+            Assert.NotNull(deserializedException);
+            Assert.Equal(originalException.Message, deserializedException.Message);
+            Assert.IsType<InvalidOperationException>(deserializedException.InnerException);
+        }
+    }
+}
