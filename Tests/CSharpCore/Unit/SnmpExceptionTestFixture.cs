a34c805c8331d15d15bedb6166c6d12f5cae77a4
Lex Li <425130+lextm@users.noreply.github.com>
2023-04-08 04:14:33 -0400

    Fixed C# 7.3 build error.

--- Tests/CSharpCore/Unit/SnmpExceptionTestFixture.cs
+++ Tests/CSharpCore/Unit/SnmpExceptionTestFixture.cs
@@ -16,17 +16,19 @@ namespace Lextm.SharpSnmpLib.Tests
 
             // Act
             // Serialize the original exception to a memory stream
-            using var memoryStream = new MemoryStream();
-            binaryFormatter.Serialize(memoryStream, originalException);
-            memoryStream.Position = 0;
+            using (var memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, originalException);
+                memoryStream.Position = 0;
 
-            // Deserialize the memory stream back into an object
-            var deserializedException = (SnmpException)binaryFormatter.Deserialize(memoryStream);
+                // Deserialize the memory stream back into an object
+                var deserializedException = (SnmpException)binaryFormatter.Deserialize(memoryStream);
 
-            // Assert
-            Assert.NotNull(deserializedException);
-            Assert.Equal(originalException.Message, deserializedException.Message);
-            Assert.IsType<InvalidOperationException>(deserializedException.InnerException);
+                // Assert
+                Assert.NotNull(deserializedException);
+                Assert.Equal(originalException.Message, deserializedException.Message);
+                Assert.IsType<InvalidOperationException>(deserializedException.InnerException);
+            }
         }
     }
 }
