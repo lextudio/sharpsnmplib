e8d9740521015574c99ba7f1ecb7cee169c10ff8
Lex Li <support@lextm.com>
2023-04-08 15:51:59 -0400

    Revised publishing settings.

--- dist.publish2nugetdotorg.bat
+++ dist.publish2nugetdotorg.bat
@@ -1,2 +1,3 @@
 .\nuget.exe update /self
 for %%f in (.\*.nupkg) do .\nuget.exe push %%f -Source https://api.nuget.org/v3/index.json
+for %%f in (.\*.snupkg) do .\nuget.exe push %%f -Source https://api.nuget.org/v3/index.json
