b676c391d71ba24d47381c9cced4df5043008e7d
Lex Li <support@lextm.com>
2024-07-30 16:13:29 -0400

    Fixed the failed test case.

--- Tests/CSharpCore/Unit/TrapV2PduTestFixture.cs
+++ Tests/CSharpCore/Unit/TrapV2PduTestFixture.cs
@@ -12,12 +12,12 @@ namespace Lextm.SharpSnmpLib.Unit
             Assert.Throws<ArgumentNullException>(() => new TrapV2Pdu(null, null));
             Assert.Throws<ArgumentNullException>(() => new TrapV2Pdu(new Tuple<int, byte[]>(0, new byte[] { 0 }), null));
             Assert.Throws<ArgumentNullException>(() => new TrapV2Pdu(0, null, 0, null));
-            Assert.Throws<ArgumentNullException>(() => new TrapV2Pdu(0, new ObjectIdentifier("1.3.6"), 0, null));            
+            Assert.Throws<ArgumentNullException>(() => new TrapV2Pdu(0, new ObjectIdentifier("1.3.6"), 0, null));
             var pdu = new TrapV2Pdu(0, new ObjectIdentifier("1.3"), 0, new List<Variable>());
-            Assert.Throws<NotSupportedException>(() => { var test = pdu.ErrorIndex; });
-            Assert.Throws<NotSupportedException>(() => { var test = pdu.ErrorStatus; });
+            Assert.Equal(Integer32.Zero, pdu.ErrorIndex);
+            Assert.Equal(Integer32.Zero, pdu.ErrorStatus);
             Assert.Throws<ArgumentNullException>(() => pdu.AppendBytesTo(null));
-            
+
             Assert.Equal("TRAP v2 PDU: request ID: 0; enterprise: 1.3; time stamp: 00:00:00; variable count: 0", pdu.ToString());
         }
     }
