90fd1939390f03ef41051febbca341ae19a6f880
Lex Li <support@lextm.com>
2025-04-24 01:49:16 -0400

    Revised publish script.

--- dist.publish2nugetdotorg.bat
+++ dist.publish2nugetdotorg.bat
@@ -1,3 +1,2 @@
-.\nuget.exe update /self
-for %%f in (.\*.nupkg) do .\nuget.exe push %%f -Source https://api.nuget.org/v3/index.json
+for %%f in (.\*.nupkg) do dotnet nuget push %%f --source https://api.nuget.org/v3/index.json -k %NUGET_API_KEY%
 @REM Symbol package is pushed in this step as well.
