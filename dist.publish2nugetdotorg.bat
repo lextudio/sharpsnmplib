0ff5e174061a430f74b2e4b9af952521ec85aaa3
Lex Li <support@lextm.com>
2025-11-03 19:07:55 -0500

    Update NuGet configuration and signing script

--- dist.publish2nugetdotorg.bat
+++ dist.publish2nugetdotorg.bat
@@ -1,2 +1,2 @@
-for %%f in (.\*.nupkg) do dotnet nuget push %%f --source https://api.nuget.org/v3/index.json -k %NUGET_API_KEY%
-@REM Symbol package is pushed in this step as well.
+nuget update /self
+for %%f in (.\*.nupkg) do nuget push %%f -Source https://www.nuget.org/api/v2/package
--- sign.nuget.ps1
+++ sign.nuget.ps1
@@ -2,6 +2,15 @@ if ($env:CI -eq "true") {
     exit 0
 }
 
+$nuget = ".\nuget.exe"
+
+if (!(Test-Path $nuget))
+{
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
+}
+
+& $nuget update /self | Write-Debug
+
 & dotnet nuget sign *.nupkg --certificate-subject-name "Yang Li" --timestamper http://timestamp.digicert.com
 & dotnet nuget verify --all *.nupkg | Write-Debug
 if ($LASTEXITCODE -ne 0)
